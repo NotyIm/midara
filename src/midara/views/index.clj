6adceb67c29c3a5bd940129a0b6ad4717f308087
Vinh <kurei@axcoto.com>
2016-01-25 22:25:47 -0800

    update index page

--- src/midara/views/index.clj
+++ src/midara/views/index.clj
@@ -5,9 +5,27 @@
 (defn index []
   (html5
     [:head
-      [:title "Midara"]
+      [:title "Midara Hook"]
+      (include-css "https://cdnjs.cloudflare.com/ajax/libs/normalize/3.0.3/normalize.css")
+      (include-css "/css/milligram.min.css")
       (include-css "/css/style.css")]
     [:body
-      [:h1 "Midara"]
-      [:p "Simply  asas setup your webhook to midara/hook"]
-    ]))
+      [:main.wrapper
+        [:nav.navigation
+          [:section.container
+            [:a.navigation-title {:href "#"} "Midara"]
+            [:ul.navigation-list.float-right
+              [:li.navigation-item
+                [:a.navigation-link {:href "docs"} "Docs"]
+              ]
+              [:li.navigation-item
+                [:a.navigation-link {:href "docs"} "Support"]
+              ]
+            ]
+          ]
+        ]
+
+        [:section.container
+          [:p "Simply setup your webhook to send push event to midara/hook"]
+        ]]]))
+
