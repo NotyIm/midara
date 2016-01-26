70e3c02e137af5eb4ccade4f5ce7f2395734488a
Vinh <kurei@axcoto.com>
2016-01-25 16:45:35 -0800

    More progress
    - Organize folder structure
    - css
    - build result page
    - doc

--- src/midara/builder/builder.clj
+++ src/midara/builder/builder.clj
@@ -6,50 +6,68 @@
               :as a
               :refer [>! <! >!! <!! go chan buffer close! thread alts! alts!! timeout]]))
 
-(defn build
-  ; Start the build process
-  [headers {:keys [repo commit] :as args}]
-  (println headers)
-  (if (= "push" (get headers "x-github-event"))
-    (let [result (repos/create-status
-                    (get-in args [:repository :owner :name])
-                    (get-in args [:repository :name])
-                    (get-in args [:head_commit :id])
-                    {:state "pending" :target_url (str "https://05f2abec.ngrok.io/build/" (get-in args [:repository :full_name]) "/" (get-in args [:head_commit :id])) :description "Midara triggered build" :context "continuous-integration/midara" :auth (System/getenv "GITHUB_AUTH_TOKEN")})]
-        (println (str "creating " result))
-        (if (= 401 (result :status))
-            (println "Fail to authenticated"))
-        )
-
-    (go (let [build-result (sh "sh" "-c" (str "echo " args repo commit "> /tmp/midara; sleep 15"))]
-      (println build-result)
-      (println (str "Exit code: " (build-result :exit)  (build-result :out)))
-      (if (= 0 (build-result :exit))
-        (do
-          (println "Build succesful")
-          (let [status-result (repos/create-status
-            (get-in args [:repository :owner :name])
-            (get-in args [:repository :name])
-            (get-in args [:head_commit :id])
-            {:state "success" :target_url (str "https://05f2abec.ngrok.io/build/" (get-in args [:repository :full_name]) "/" (get-in args [:head_commit :id])) :description "Midara build succeed" :context "continuous-integration/midara" :auth (System/getenv "GITHUB_AUTH_TOKEN")})]
-              (println (str "creating result: " status-result)))
-        (println (build-result :out)))
-    )))))
+(defn -description
+  ; Get description for build
+  [state]
+  (if (= state "pending")
+    "Midara triggered build"
+    (if (= state "sucess")
+      "Midara build succeed"
+      (str "Midara build: " state))
+  )
+)
 
 (defn set-status
   ; Set status of commit
-  [owner repo-name commit status]
-  (let [result (repos/create-status owner repo-name commit)]
-    (println (str "creating " result))
-    (if (= 401 (result :status))
-        (println "Fail to authenticated"))
-    ))
+  [owner repo-name commit state]
+  (let [b {:state state :target_url (str "https://05f2abec.ngrok.io/build/" owner "/" repo-name "/" commit) :description (-description state) :context "continuous-integration/midara" :auth (System/getenv "GITHUB_AUTH_TOKEN")}]
+    (repos/create-status owner repo-name commit b)
+  ))
 
 (defn -execute
   ; Execute build script.
   [repo]
   (println repo)
   )
+
+(defn -execute
+  ; Execute build
+  [args]
+  (let [owner (get-in args [:repository :owner :name])
+          name (get-in args [:repository :name])
+          commit (get-in args [:head_commit :id])]
+    (def pwd (System/getProperty "user.dir"))
+    (def workspace (str pwd "/workspace"))
+    (def workdir (clojure.string/join "/" [workspace owner name commit]))
+    (def build-log (str workdir "/build/midara-build.log"))
+    (println (map #(.mkdir (java.io.File. %))
+          [workspace (clojure.string/join "/" [workspace owner]) (clojure.string/join "/" [workspace owner name]) workdir (clojure.string/join "/" [workdir "src"]) (clojure.string/join "/" [workdir "build"])]))
+
+    (println "Build with command: echo " args  build-log "; sleep 15"))
+    (println (System/getProperty "user.dir"))
+    (println (-> (java.io.File. ".") .getAbsolutePath))
+    (sh "sh" "-c" (str "echo " args "> " build-log "; sleep 15")))
+
+(defn build
+  ; Start the build process
+  [headers {:keys [repo commit] :as args}]
+  (if (= "push" (get headers "x-github-event"))
+    (let [owner (get-in args [:repository :owner :name])
+          name (get-in args [:repository :name])
+          commit (get-in args [:head_commit :id])]
+      (let [result (set-status owner name commit "pending")]
+        (println (str "creating " result))
+        (if (= 200 (result :status))
+          (println "Fail to create status. Abandon build")
+          (go (let [build-result (-execute args)]
+            (if (= 0 (build-result :exit))
+              (do
+                (println "Build succesful")
+                (let [status-result (set-status owner name commit "success")]
+                  (println (str "creating result: " status-result))))
+              (println "Build fail")
+            )))
+    )))))
 
 (defn start
   ; Start the build process
--- src/midara/views/build.clj
+++ src/midara/views/build.clj
@@ -7,9 +7,34 @@
 (defn view
   ; render view buld page
   [owner repo rev]
+
   (html5
     [:head
-      [:title (str "Build for " owner "/" repo "/" rev)]
+      [:title "Midara Hook"]
+      (include-css "https://cdnjs.cloudflare.com/ajax/libs/normalize/3.0.3/normalize.css")
+      (include-css "/css/milligram.min.css")
       (include-css "/css/style.css")]
     [:body
-      [:div "file content"]]))
+      [:main.wrapper
+        [:nav.navigation
+          [:section.container
+            [:a.navigation-title {:href "#"} "Midara"]
+            [:ul.navigation-list.float-right
+              [:li.navigation-item
+                [:a.navigation-link {:href "docs"} "Docs"]
+              ]
+              [:li.navigation-item
+                [:a.navigation-link {:href "docs"} "Support"]
+              ]
+            ]
+          ]
+        ]
+
+        [:section.container
+          [:h5.title (str "Build " owner "/" repo ": " rev)]
+          [:div.example
+            [:pre.code.prettyprint.prettyprinted (slurp (clojure.string/join "/" ["workspace" owner repo rev "build/midara-build.log"]))]]
+        ]
+      ]]
+  )
+)
